14f83ab6472631ba40f0fade4a2fdcab7205f251
Neil <chen2847006@gmail.com>
2020-08-17 18:20:13 +0800

    reset tappay.

--- lib/tappay.dart
+++ lib/tappay.dart
@@ -1,21 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:tappay/model/valid_response.dart';
 
 import 'model/credit_card.dart';
 
 class Tappay {
   static const MethodChannel _channel = const MethodChannel('tappay/bridge');
-
-  static Future<String> get platformVersion async {
-    return await _channel.invokeMethod('getDirectPayToken', {
-      "cardNumber": "4242424242424242",
-      "dueMonth": "01",
-      "dueYear": "23",
-      "CCV": "123"
-    });
-  }
 
   // -------------------------------------------
   void initTapPay(int id, String appKey) async {
@@ -28,31 +18,22 @@ class Tappay {
   }
 
   // -------------------------------------------
-  Future<void> cardValid(CreditCard card) async {
-    try {
-      await _channel.invokeMethod('cardValid', {
-        'cardNumber': card.number,
-        'dueMonth': card.dueMonth,
-        'dueYear': card.dueYear,
-        'CCV': card.ccv
-      });
-      return ValidResponse();
-    } catch (e) {
-      print('>> $e');
-    }
+  Future<bool> cardValid(CreditCard card) async {
+    return await _channel.invokeMethod('cardValid', {
+      'cardNumber': card.number,
+      'dueMonth': card.dueMonth,
+      'dueYear': card.dueYear,
+      'CCV': card.ccv
+    });
   }
 
   // -------------------------------------------
   Future<String> getToken(CreditCard card) async {
-    try {
-      return await _channel.invokeMethod('getDirectPayToken', {
-        'cardNumber': card.number,
-        'dueMonth': card.dueMonth,
-        'dueYear': card.dueYear,
-        'CCV': card.ccv
-      });
-    } catch (e) {
-      print(e);
-    }
+    return await _channel.invokeMethod('getDirectPayToken', {
+      'cardNumber': card.number,
+      'dueMonth': card.dueMonth,
+      'dueYear': card.dueYear,
+      'CCV': card.ccv
+    });
   }
 }
