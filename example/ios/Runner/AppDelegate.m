9433935a3fcf36c77028c9932c17c5c31034ca37
Neil <chen2847006@gmail.com>
2020-08-19 15:18:50 +0800

    fix iOS error.

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import <TPDirect/TPDirect.h>
 
 @implementation AppDelegate
 
@@ -7,6 +8,12 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+  // set TapPay.
+  [TPDSetup setWithAppId:123 withAppKey:@"AppKey" withServerType:TPDServer_SandBox];
+//  [[TPDSetup shareInstance] setupIDFA:@""];
+  [[TPDSetup shareInstance] serverSync];
+    
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
