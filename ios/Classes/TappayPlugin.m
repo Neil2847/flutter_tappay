e454887b3ee05137eeb66264c2842c71f0c9a3ca
Neil <chen2847006@gmail.com>
2020-08-19 12:55:15 +0800

    set ios.

--- ios/Classes/TappayPlugin.m
+++ ios/Classes/TappayPlugin.m
@@ -3,14 +3,14 @@
 @implementation TappayPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"tappay"
+      methodChannelWithName:@"tappay/bridge"
             binaryMessenger:[registrar messenger]];
   TappayPlugin* instance = [[TappayPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+  if ([@"getDirectPayToken" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else {
     result(FlutterMethodNotImplemented);
